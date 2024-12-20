fc9a95ee83f716cd0f77c395b214b1103f4c2e27
EduardHairapetyan <xunushinak@gmail.com>
2024-12-20 21:09:47 +0400

    ref

--- Lesson 2/2.1.js
+++ Lesson 2/2.1.js
@@ -1,1 +1,13 @@
-// Didn't understand exercise
+const emailList = [
+    "example@gmail.com",
+    "somename@mail.ru",
+    "testmail1@yandex.com",
+    "testmail2@yandex.com",
+];
+
+const emailInput = document.querySelector('#email');
+
+const randomIndex = Math.floor(Math.random() * emailList.length);
+const placeholderEmail = emailList[randomIndex];
+
+emailInput.placeholder = placeholderEmail;
